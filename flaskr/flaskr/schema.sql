d7e9b5bedcda4a7fd3666754e1c9cabce41caa1e
Rodrigo R. Ribeiro <rodrigo.rufino@inatel.br>
2018-03-13 16:53:51 -0300

    Mudanca banco de dados login

--- flaskr/flaskr/schema.sql
+++ flaskr/flaskr/schema.sql
@@ -4,3 +4,9 @@ create table entries (
   title text not null,
   'text' text not null
 );
+
+create table users (
+  id integer primary key autoincrement,
+  username text not null,
+  password text not null
+)
